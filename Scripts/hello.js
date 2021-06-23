9d6f297f7c46b93612e4b9cdf5cc29941f9bb6eb
xiaoweihua <xiaoweihua@cnhnkj.com>
2021-06-23 19:33:05 +0800

    hello.js 学习

--- Scripts/hello.js
+++ Scripts/hello.js
@@ -1,10 +1,22 @@
 // 引入http模块(require相当于java中的import)
-var http = require('http');
+const http = require('http');
+const querystring = require('querystring');
 
 // 创建服务并监听8080端口(10.10.21.72是我本机的IP地址)
-var app = http.createServer(function(req,res){
-    // 设置响应头
-    res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
-    // 响应的内容部分
-    res.end('My first nodejs application!');
-}).listen(8080,'localhost');
+const sever = http.createServer((req, res) => {
+    const method = req.method;    
+    console.log('method', method);
+    const url = req.url;
+    console.log('url', url);
+    req.query = querystring.parse(url.split('?')[1]);
+    console.log('query', req.query);
+
+    res.end(
+        JSON.stringify(req.query)
+    );
+});
+
+sever.listen(8080, (err) => {
+    if (err) throw err;
+    console.log("sever running at port 8080");
+});
