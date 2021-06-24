8e24e7cc93f999e05baffda02e5a71d0d213135a
xiaoweihua <xiaoweihua@cnhnkj.com>
2021-06-24 11:00:01 +0800

    weibo_checkIn更新

--- Scripts/weibo_checkIn.js
+++ Scripts/weibo_checkIn.js
@@ -47,7 +47,7 @@ function request(url, data, method = "get") {
 
   return fetch(url, {
     method,
-    body: JSON.stringify(data),
+    // body: JSON.stringify(data),
     headers: {
       ua: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
       cookie:
