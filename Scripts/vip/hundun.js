9c382a482cb35e18d42b58398f130de0be8c33b1
Evans.Xiao <1019459067@qq.com>
2023-06-04 18:16:45 +0800

    Update hundun.js

--- Scripts/vip/hundun.js
+++ Scripts/vip/hundun.js
@@ -1,14 +1,14 @@
 /******************************
 脚本功能：混沌 解锁VIP、缓存、画中画
 软件版本：6.12.0
-更新时间：2023-5-22
+更新时间：2023-6-4
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
 
 [rewrite_local]
 
 ^https:\/\/course\.hundun\.cn\/course\/expire_course_list url reject
-^http[s]?:\/\/(course|user)\.hundun\.cn\/(get_course_detail|get_user_info)? url script-response-body https://raw.githubusercontent.com/1019459067/Quantumult-X/master/Scripts/vip/huntun.js
+^http[s]?:\/\/(course|user)\.hundun\.cn\/(get_course_detail|get_user_info)? url script-response-body https://raw.githubusercontent.com/1019459067/Quantumult-X/master/Scripts/vip/hundun.js
 
 [mitm]
 
@@ -26,12 +26,6 @@ if (url.indexOf('/get_course_detail') != -1) {
     obj.data.course_meta.allow_download = 1;
     obj.data.course_meta.allow_play = 1;
     obj.data.course_meta.expire_time = "2058-10-20";
-
-    obj.data.playback_button.join_name = "人民币玩家";
-    obj.data.playback_button.page_url = "";
-    obj.data.playback_button.h5_url = "";
-    obj.data.playback_button.buy_item = "";
-    obj.data.playback_button.btn_name = "";
 
     obj.data.free_trial_info = "";
 }
