f9dc1f840389fd33e0e40c7f457551fbcd4bcef8
Monkey <>
2023-06-18 15:33:02 +0800

    优化格式

--- Free/5.scope.py
+++ Free/5.scope.py
@@ -6,16 +6,16 @@ import json
 import ssr_utils
 
 headers = {
-    "Host": "api-2.quickg.cc",
-    "Authorization": "Token d327b0df4873e3e2020578ae77083ae059634ca7",
-    "channel": "GW",
-    "appVersion": "2.1.7",
-    "platform": "ios",
-    "imei": "b8d5f841bdd44230b8b155b3b9317f31",
-    "Content-Length": "0",
-    "User-Agent": "Telescope/219 CFNetwork/1335.0.3 Darwin/21.6.0",
-    "Accept": "application/json",
-    "systemVersion": "15.6.1",
+    "Host" : "api-2.quickg.cc",
+    "Authorization" : "Token d327b0df4873e3e2020578ae77083ae059634ca7",
+    "channel" : "GW",
+    "appVersion" : "2.1.7",
+    "platform" : "ios",
+    "imei" : "b8d5f841bdd44230b8b155b3b9317f31",
+    "Content-Length" : "0",
+    "User-Agent" : "Telescope/219 CFNetwork/1335.0.3 Darwin/21.6.0",
+    "Accept" : "application/json",
+    "systemVersion" : "15.6.1"
 }
 
 merge = []
@@ -42,19 +42,18 @@ try:
     
         # 将节点信息转换为SS节点格式的字符串
         ssr = ssr_utils.SSR()
-        ssr.set(server=server_address,
-            port=server_port,
-            method=method,
-            password=password,
-            protocol=protocol,
-            proto_param=protocol_param,
-            obfs=obfs,
-            obfs_param=obfs_param,
-            remarks=remarks
-            )    
-        # 打印SS节点字符串    
+        ssr.set(server = server_address,
+                port = server_port,
+                method = method,
+                password = password,
+                protocol = protocol,
+                proto_param = protocol_param,
+                obfs = obfs,
+                obfs_param = obfs_param,
+                remarks = remarks)
+        #打印SS节点字符串
         print(ssr.url)
-        merge.append(ssr.url) 
+        merge.append(ssr.url)
     
 except:
     traceback.print_exc()
